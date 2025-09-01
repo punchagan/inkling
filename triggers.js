8dec7daf15782c9e04279d83e038b54b8655bb7a
Puneeth Chaganti <punchagan@muse-amuse.in>
2025-09-01 13:47:42 +0530

    Tweak menu item for sending to all contacts

--- triggers.js
+++ triggers.js
@@ -2,7 +2,7 @@ function onOpen() {
   SpreadsheetApp.getUi()
     .createMenu("Inkling")
     .addItem("Send test to me", "sendTestToMe")
-    .addItem("Send from Doc", "sendEmailsFromDoc")
+    .addItem("Send to ALL CONTACTS!", "sendEmailsFromDoc")
     .addSeparator()
     .addItem("Open Web View", "_openWebViewDialog")
     .addToUi();
